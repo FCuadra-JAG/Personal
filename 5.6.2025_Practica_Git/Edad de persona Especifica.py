d06f28fb167c416eb19681dc59a58f018a23dc30
FCuadra-JAG <f.cuadra@uamv.edu.ni>
2025-05-06 09:55:55 -0600

    Add files via upload

---
+++ 5.6.2025_Practica_Git/Edad de persona Especifica.py
@@ -0,0 +1,6 @@
+Año_Reciente = int(input(" Escriba el año actual : "))
+Año_Nacimiento = int(input(" escriba el año de nacimiento : "))
+
+Resultado=Año_Reciente-Año_Nacimiento
+
+print(f"El individuo tiene {Resultado} años")
